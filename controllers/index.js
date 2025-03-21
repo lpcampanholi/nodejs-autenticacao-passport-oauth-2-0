334bf98c3fd5c96dea7cd03943b3991ac211b5a7
Luís Plinio <luis.santos@sgsistemas.com.br>
2025-03-21 19:43:09 -0300

    Implementação de SignUp e Login

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,43 @@
+const User = require("../models/user");
+
 exports.showIndex = (req, res, next) => {
-    res.render('index')
-}
+  res.render("index");
+};
 
 exports.showPageSignUp = (req, res, next) => {
-    res.render('signUp')
-}
+  res.render("signup");
+};
 
 exports.showMembersPage = (req, res) => {
-    res.render('members')
-}
+  res.render("members");
+};
 
 exports.get404Page = (req, res, next) => {
-    res.status(404).render('404')
-}
+  res.status(404).render("404");
+};
+
+exports.signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const user = new User(username, email, password);
+  try {
+    await user.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/signup");
+  }
+};
+
+exports.login = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = User.findOne(email, password);
+  try {
+    if (user) {
+      res.redirect("/members");
+    }
+    res.render("index");
+  } catch (error) {
+    console.log(error);
+    res.render("index");
+  }
+};
